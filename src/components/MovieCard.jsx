3d9cfabed7a80e6960ead609ee194915f0d816e7
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-10 13:12:13 -0500

    fixed no image poster rendering now

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -4,7 +4,7 @@ const MovieCard = ({ movie:{ title, vote_average, poster_path, release_date, ori
     <>
     <div className="movie-card">
         <img src={poster_path ? 
-        `https://image.tmdb.org/t/p/w500/${poster_path}` : '/no-movie.png'} 
+        `https://image.tmdb.org/t/p/w500/${poster_path}` : './public/img/no-movie.png'} 
         alt={title}/>
         <div className="mt-4">
             <h3>{title}</h3>
