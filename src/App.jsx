6d18e842e84ddda324118535d8ae07fd2226e69b
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-10 10:44:32 -0500

    added header

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,15 @@ function App() {
 
   return (
     <>
-     
+    <main>
+      <div className='pattern'>
+        <div className='wrapper'>
+          <header>
+            <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
+          </header>
+        </div>
+      </div>
+    </main>
     </>
   )
 }
