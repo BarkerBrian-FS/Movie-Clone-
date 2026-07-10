c512eb7612b5667c4aaf84517ec025c5c3cc1590
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-10 11:07:28 -0500

    added all movie images and icons

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
           <header>
             <h1>Find <span className='text-gradient'>Movies</span> You'll Enjoy Without the Hassle</h1>
           </header>
+          <p>Search</p>
         </div>
       </div>
     </main>
