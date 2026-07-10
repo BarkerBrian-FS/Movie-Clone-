1d1af6a34687da4d19f3fef17e710a39bb3bd1c7
BarkerBrian-FS <bmbarker@student.fullsail.edu>
2026-07-10 11:33:09 -0500

    finished header section

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
 
-const Search = (searchTerm, setSearchTerm) => {
+const Search = ({searchTerm, setSearchTerm}) => {
   return (
     <>
-    <div className='text-white text-3xl'>Search</div>
+    <div className='search'>
+        <div>
+            <img src='./public/img/search.svg' alt='search icon' /> 
+            <input
+            type='text'
+            placeholder='Search through thousands of movies'
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)} />
+        </div>
+    </div>
     </>
   )
 }
